a0bdd042f1811cb65a9b4f62060807ffa3bc2617
yalagalasrinivas <yalagala@jrcompliance.com>
2022-02-21 16:33:41 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHfeiGb-qLY4CgkKtrGV9plcWcPj2VBQQ",
+  authDomain: "jrcrm-4f580.firebaseapp.com",
+  databaseURL: "https://jrcrm-4f580-default-rtdb.firebaseio.com",
+  projectId: "jrcrm-4f580",
+  storageBucket: "jrcrm-4f580.appspot.com",
+  messagingSenderId: "482749695187",
+  appId: "1:482749695187:web:7a15babbc145e16c36c026",
+  measurementId: "G-1DG924Y1CR"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("Query");
+module.exports = User;
